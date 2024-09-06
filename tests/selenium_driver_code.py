d1dbe8b957c65ecff6a3cf7ae93823b4f8230681
RainbowRecipe <95683693+RainbowRecipe@users.noreply.github.com>
2024-09-06 20:18:35 +0530

    Updated Selenium script to work on CircleCI

--- tests/selenium_driver_code.py
+++ tests/selenium_driver_code.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -8,11 +10,24 @@ import traceback
 import math
 import os
 
-driver = webdriver.Chrome()
+#driver = webdriver.Chrome()
 
 # Open the Flask app URL
 app_url = 'https://circleci.azurewebsites.net/'
 print("Navigating to:", app_url)
+#driver.get(app_url)
+#service = Service(executable_path=r'/usr/local/bin/chromedriver')
+options = webdriver.ChromeOptions()
+options.add_argument('enable-automation')
+options.add_argument('--headless')
+options.add_argument('--no-sandbox')
+options.add_argument("--disable-extensions")
+options.add_argument("--dns-prefetch-disable")
+options.add_argument("--disable-gpu")
+#options.add_argument('--disable-dev-shm-usage')
+options.add_argument('--remote-debugging-pipe')
+options.add_experimental_option("excludeSwitches", ['enable-automation'])
+driver = webdriver.Chrome(options=options)
 driver.get(app_url)
 
 
